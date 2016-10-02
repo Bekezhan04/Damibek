edcbad4a66dd21b074dfd550835a3de9360e4882
bekezhan ainakulov <be04@yandex.kz>
2016-10-02 16:23:43 +0600

    First commit

---
+++ Damibek/Damibek/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Damibek.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult about()
+        {
+            return View();
+        }
+
+        public ActionResult services()
+        {
+            return View();
+        }
+
+        public ActionResult portfolio()
+        {
+            return View();
+        }
+        public ActionResult blog()
+        {
+            return View();
+        }
+        public ActionResult contact()
+        {
+            return View();
+        }
+
+
+    }
+}
